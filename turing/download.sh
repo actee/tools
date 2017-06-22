c3b1828dbb75f569ac7a9502a07f41a08042125f
actee <actee@sapo.pt>
2017-06-22 21:35:50 +0100

    Create download.sh

---
+++ turing/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# or change it to your shell
+# copy-paste and execute ( probably chmod +x )
+curl -s -o fileio.c https://raw.githubusercontent.com/actee/tools/master/turing/fileio.c
+curl -s -o fileio.h https://raw.githubusercontent.com/actee/tools/master/turing/fileio.h
+curl -s -o main.c https://raw.githubusercontent.com/actee/tools/master/turing/main.c
+curl -s -o makefile https://raw.githubusercontent.com/actee/tools/master/turing/makefile
+curl -s -o t_states https://raw.githubusercontent.com/actee/tools/master/turing/t_states
